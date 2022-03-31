eda447683dc508a89cfc0184f6e6741e206bbeab
ardor-zhang <ardor-zhang@outlook.com>
2022-03-31 09:53:32 +0800

    feat: leetcode

--- src/Chapter-1-Basic/03-Link/01-book/07-reverse-link.js
+++ src/Chapter-1-Basic/03-Link/01-book/07-reverse-link.js
@@ -27,6 +27,18 @@ function reverse_v2(link) {
   return reverse;
 };
 
-const reversedLink = reverse_v1(first);
-console.log(JSON.stringify(reversedLink, null, 2));
-console.log(JSON.stringify(reverse_v2(first), null, 2));
+function reverse_v3(link) {
+  if (link === null || link.next === null) {
+    return link;
+  };
+  
+  const reversed = reverse_v3(link.next);
+  link.next.next = link;
+  link.next = null;
+  return reversed;
+};
+
+// const reversedLink = reverse_v1(first);
+// console.log(JSON.stringify(reversedLink, null, 2));
+// console.log(JSON.stringify(reverse_v2(first), null, 2));
+console.log(JSON.stringify(reverse_v3(first), null, 2));
--- src/Chapter-1-Basic/03-Link/02-leetcode/01-reverse-link-n-m.js
+++ src/Chapter-1-Basic/03-Link/02-leetcode/01-reverse-link-n-m.js
@@ -54,3 +54,66 @@ function reverseNM_v2(link, n, m) {
 // console.log(JSON.stringify(r, null, 2));
 console.log(JSON.stringify(reverseNM_v2(first, 1, 3), null, 2));
 console.log(JSON.stringify(first, null, 2));
+
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} left
+ * @param {number} right
+ * @return {ListNode}
+ */
+// 反转链表
+const reverseLink = function(head) {
+  let curr = null;
+  let prev = head;
+  while(prev) {
+    const next = prev.next;
+    prev.next = curr;
+    curr = prev;
+    prev = next;
+  };
+  return curr;
+}
+
+const reverseBetween = function(head, left, right) {
+  // 因为头节点有可能发生变化，使用虚拟头节点可以避免复杂的分类讨论
+  const dummyNode = new ListNode(-1);
+  dummyNode.next = head;
+
+  let pre = dummyNode;
+  // 第 1 步：从虚拟头节点走 left - 1 步，来到 left 节点的前一个节点
+  // 建议写在 for 循环里，语义清晰
+  for (let i = 0; i < left - 1; i++) {
+      pre = pre.next;
+  }
+
+  // 第 2 步：从 pre 再走 right - left + 1 步，来到 right 节点
+  let rightNode = pre;
+  for (let i = 0; i < right - left + 1; i++) {
+      rightNode = rightNode.next;
+  }
+
+  // 第 3 步：切断出一个子链表（截取链表）
+  let leftNode = pre.next;
+  let curr = rightNode.next;
+
+  // 注意：切断链接
+  pre.next = null;
+  rightNode.next = null;
+
+  // 第 4 步：同第 206 题，反转链表的子区间
+  reverseLink(leftNode);
+
+  // 第 5 步：接回到原来的链表中
+  pre.next = rightNode;
+  leftNode.next = curr;
+  return dummyNode.next;
+};
