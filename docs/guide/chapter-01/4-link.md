571286118c9a2b89d9e431ce3e3aa38381107012
zhangpaopao <zhangpaopao0609@gmail.com>
2022-09-10 09:35:19 +0800

    fix: delete k node

--- docs/guide/chapter-01/4-link.md
+++ docs/guide/chapter-01/4-link.md
@@ -419,25 +419,21 @@ console.log(JSON.stringify(first, null, 2));
    :::details 点击查看参考答案
 
    ```js
-   function deleteKNode_v2(link, k) {
+   function deleteKNode(link, k) {
      if (link === null) throw Error('link is null');
-     if (k <= 0) throw Error('k should bgt 0');
-
-     // 虚拟头结点
-     let curr = new Node(null, link);
-     // 循环 k - 1 次，因为要找的是第 k-1 个结点，所以循环 k-1 次就能找到
-     for (let i = 1; i < k - 1; i++) {
+     if (k <= 0) throw Error('k should ght 0');
+     if (k === 1) return link.next;
+     let curr = link;
+     // 找到第 k-1 个结点，因此循环 k-2 次
+     for (let i = 0; i < k - 2; i++) {
        curr = curr.next;
-       // 如果已经走到了尾结点
        if (curr.next === null) return link;
      }
-     // 删除第 k 个结点
      curr.next = curr.next.next;
-
      return link;
    }
 
-   console.log(JSON.stringify(deleteKNode_v2(first, 11), null, 2));
+   console.log(JSON.stringify(deleteKNode(first, 11), null, 2));
    ```
 
    :::
--- src/chapter-01-basic/04-link/02-practice/01-after-class/01-delete-k-node.js
+++ src/chapter-01-basic/04-link/02-practice/01-after-class/01-delete-k-node.js
@@ -1,45 +1,19 @@
 // 编写一个方法 delete()，接收一个参数 k，删除链表的第 k 个元素，如果它存在的话
-const { Node, link } = require('./00-node');
+const { link } = require('./00-node');
 let first = link;
 
-// function deleteKNode(link, k) {
-//   if(link === null) throw Error('link is null');
-//   if(k <= 0) throw Error('k should bgt 0');
-//   if(k === 1) {
-//     return link.next;
-//   }
-//   // 头结点
-//   let curr = link;
-//   // 循环 k - 2 次，因为要找的是第 k-1 个结点，所以循环 k-2 次就能找到  
-//   for (let i = 1; i < k - 1; i++) {
-//     curr = curr.next;
-//     // 如果已经走到了尾结点
-//     if(curr.next === null) return link;
-//   }
-//   // 删除第 k 个结点
-//   curr.next = curr.next.next;
-
-//   return link;
-// }
-
-// console.log(JSON.stringify(deleteKNode(first, 11), null, 2));
-
-function deleteKNode_v2(link, k) {
-  if(link === null) throw Error('link is null');
-  if(k <= 0) throw Error('k should bgt 0');
-
-  // 虚拟头结点
-  let curr = new Node(null, link);
-  // 循环 k - 1 次，因为要找的是第 k-1 个结点，所以循环 k-1 次就能找到  
-  for (let i = 1; i < k - 1; i++) {
+function deleteKNode(link, k) {
+  if (link === null) throw Error('link is null');
+  if (k <= 0) throw Error('k should ght 0');
+  if (k === 1) return link.next;
+  let curr = link;
+  // 找到第 k-1 个结点，因此循环 k-2 次
+  for (let i = 0; i < k - 2; i++) {
     curr = curr.next;
-    // 如果已经走到了尾结点
-    if(curr.next === null) return link;
+    if (curr.next === null) return link;
   }
-  // 删除第 k 个结点
   curr.next = curr.next.next;
-
   return link;
 }
 
-console.log(JSON.stringify(deleteKNode_v2(first, 11), null, 2));
+console.log(JSON.stringify(deleteKNode(first, 11), null, 2));
